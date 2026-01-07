9fc5dec5bac718fc2abc0f86a80ccf0973dd7c87
Shaykov Alexey <kayfolom@inbox.ru>
2026-01-07 13:48:56 +0300

    feat: initialize flight tracking web app with React, TypeScript, and Vite
    - Add ESLint configuration for TypeScript and React
    - Create index.html as the entry point for the application
    - Set up package.json with necessary dependencies and scripts
    - Include favicon.svg for the application
    - Implement Home component as the main page
    - Add global CSS styles using Tailwind CSS
    - Set up main.tsx to render the application with React Router
    - Configure TypeScript settings for the application and Node
    - Create Vite configuration for building the application

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import Home from './Home'
+
+ import './index.css'
+
+createRoot(document.getElementById('root')!).render( // ! non-null assertion
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route 
+        path="/"
+        element={<Home />}
+      />
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
