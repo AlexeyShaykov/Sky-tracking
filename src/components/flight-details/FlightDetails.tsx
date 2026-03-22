f3daf3fef47a43cd5f7d345718b4c723240ae427
Shaykov Alexey <kayfolom@inbox.ru>
2026-03-22 18:06:10 +0300

    main |feat: remove unused onFollow and onMore props from FlightActions component

--- src/components/flight-details/FlightDetails.tsx
+++ src/components/flight-details/FlightDetails.tsx
@@ -55,8 +55,6 @@ export const FlightDetails = () => {
           <FlightInformation flight={flight} />
 
           <FlightActions
-            onFollow={() => {}}
-            onMore={() => {}}
             flight={flight}
           />
         </div>
